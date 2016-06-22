54cc3074f14dbe329b9c7133224f67daaea4c5dd
darbymanning <darby@creativelittledots.co.uk>
2016-06-22 14:08:42 +0100

    del: removed app-min.js compiled by codekit

--- iKnowChurch.safariextension/min/app-min.js
+++
@@ -1,1 +0,0 @@
-var customScript=document.createElement("script");customScript.setAttribute("src","//iknow.code-lab.co.uk/scripts/app.min.js"),document.querySelector("body").appendChild(customScript);
